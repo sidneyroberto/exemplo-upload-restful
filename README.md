4bf99468ab91e321226890d393615082c06fdee0
Sidney Sousa <sidney.sousa@ifms.edu.br>
2021-03-30 16:13:34 -0400

    commit inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+# exemplo-upload-restful
+
+Exemplo de serviço RESTful de upload de arquivos com Node, Express e TypeScript.
+
+Para executar o projeto, primeiro instale as dependências:
+
+#### `npm i`
+
+Após, execute o projeto:
+
+#### `npm run dev`
---
+++ src/routes/upload.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+import * as path from 'path'
+import * as fs from 'fs'
+
+export const uploadRouter = Router()
+
+uploadRouter.post('/', (req, res) => {
+    if (!req.files || Object.keys(req.files).length == 0) {
+        return res.status(404).send('Nenhum arquivo recebido')
+    }
+
+    const nomesArquivos = Object.keys(req.files)
+    const diretorio = path.join(__dirname, '..', '..', 'arquivos')
+    if (!fs.existsSync(diretorio)) {
+        fs.mkdirSync(diretorio)
+    }
+
+    nomesArquivos.forEach(arquivo => {
+        const objArquivo = req.files[arquivo]
+        const nomeArquivo = objArquivo['name']
+        const conteudoArquivo = objArquivo['data']
+
+        const caminhoArquivo = path.join(diretorio, nomeArquivo)
+        fs.writeFileSync(caminhoArquivo, conteudoArquivo)
+    })
+
+    res.send('Arquivos gravados no servidor')
+})
